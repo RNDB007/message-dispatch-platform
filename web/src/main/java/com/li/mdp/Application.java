a24f4d9b8f3d67c7f0642f5c14293350dc9dfc09
li <a374859311@gmail.com>
2023-07-30 21:56:30 +0800

    接入springBoot

---
+++ web/src/main/java/com/li/mdp/Application.java
@@ -0,0 +1,16 @@
+package com.li.mdp.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Joe Lee
+ * @date
+ * @Description
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
---
+++ web/src/main/java/com/li/mdp/web/TestController.java
@@ -0,0 +1,7 @@
+package com.li.mdp.web;/**
+ *
+ * @author Joe Lee
+ * @date 
+ * @Description 
+ */public class TestController {
+}
